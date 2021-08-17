b2fd08190a5c9b39b0a75159355c825cd2e577dc
Mariela <mayercandelo.1985@gmail.com>
2021-08-17 10:35:19 -0500

    mari

--- src/view/404.js
+++ src/view/404.js
@@ -1,5 +1,5 @@
 export default () => {
-    const viewError = `<h2>Esta página no existe</h2>`;
+    const viewError = `<h2>"Esta página no existe"</h2>`;
     const divElement = document.createElement("div");
     divElement.innerHTML = viewError;
     return divElement;
