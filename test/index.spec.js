e9df469279bf8a202012700b5d1ff7d48560c8cf
Mariela <85802837+mayer-2906@users.noreply.github.com>
2021-08-30 20:39:19 -0500

    cambios de 30/08 con daniela

--- test/index.spec.js
+++ test/index.spec.js
@@ -1,4 +1,8 @@
 // importamos la funcion que vamos a testear
+<<<<<<< HEAD
 import { signIn, register, signOutDelicious, logInWithGoogle} from '../src/functionFirebase';
 
+=======
+import { register, signIn, signOutDelicious, logInWithGoogle } from '../src/functionFirebase.js';
+>>>>>>> 46f8e9c3d4d976d440fc86865546be3f86fa6367
 
