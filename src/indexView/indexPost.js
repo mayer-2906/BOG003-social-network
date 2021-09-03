ff8eac344f77df8df872d65c07112318c2482ae9
Daniela Funes <danielafunes1970@gmail.com>
2021-09-02 23:59:09 -0500

    primera parte de función editar

--- src/indexView/indexPost.js
+++ src/indexView/indexPost.js
@@ -1,36 +1,125 @@
 import { signOutDelicious } from '../functionFirebase.js';
 import { post } from '../view/post.js';
 
-// función del template post
 export const functionPost = () => {
   const divElement = document.createElement('div');
   divElement.innerHTML = post();
   const buttonSignOut = divElement.querySelector('#signOut');
   const inputPostUser = divElement.querySelector('#inputPostear');
-  const containerPosts = divElement.querySelector('#containerPost');
+  const searchInput = divElement.querySelector('#searchInput');
+  let enableEdit = true;
+  document.addEventListener('DOMContentLoaded', async () => {
+    /* eslint-disable */
+    // console.log('cargo indexPost');
+    loadPost();
+  });
 
   buttonSignOut.addEventListener('click', () => {
     signOutDelicious()
       .then(() => {
+        /* eslint-disable */
+        // console.log('sign out');
         window.location.href = '#/initial';
+      }).catch((error) => {
+      //console.log('sign out');
       });
   });
 
-  const createPost = (data) => {
+  inputPostUser.addEventListener('click', async () => {
+    // window.location.href = '#/post';
+    divElement.querySelector('#containerPost').innerHTML = '';
+    const recipe = divElement.querySelector('#recipePostear').value;
+    const fecha = new Date();
+    const date = `${fecha.getDate()}/${fecha.getMonth() + 1}/${fecha.getFullYear()}`;
+    var db = firebase.firestore();
     const user = firebase.auth().currentUser;
+    // let userLogin='';
+    await db.collection("post").add({
+      name: user.displayName,
+      recipe: recipe,
+      fecha: date,
+      user: user.uid,
+      })
+      .then(() => {
+          loadPost();
+          divElement.querySelector('#recipePostear').value='';
+          //console.log("Document written with ID: ", docRef.id);
+      })
+      .catch(() => {
+          alert('Lo sentimos no pudimos agregar tu post, intenta de nuevo');
+          divElement.querySelector('#recipePostear').value='';
+      });
+    });
+
+
+  const loadPost = () => {
+    const db = firebase.firestore();
+    firebase.auth().onAuthStateChanged(user => {
+      if(user){
+        db.collection('post')
+          .get()
+          .then((snapshot) => {
+            const helloUser = divElement.querySelector('#helloUser');
+            helloUser.innerHTML = `Hola ${user.displayName}`; 
+            showPost(snapshot.docs, user)
+            editing();
+          })
+      }
+    });
+  }
+
+  const showPost = (data, user) => {
+    if(data.length){
+      let addHtml = '';
+      data.forEach(post => {
+        const postData = post.data();
+        const idPost = post.id
+        addHtml += createPost(postData, user, idPost);
+      });
+      const divcontainerPost = document.createElement('div');
+      divcontainerPost.innerHTML = addHtml;
+      divElement.querySelector('#containerPost').innerHTML='';
+      divElement.querySelector('#containerPost').appendChild(divcontainerPost);
+    }
+  }
+  const editing = () => {
+    const inputEditar = divElement.querySelectorAll(".inputEditDesktop");
+    console.log('esto son los inputs ', inputEditar);
+    inputEditar.forEach(editButton => {
+      editButton.addEventListener('click',async (e)=>{
+        if (enableEdit) {
+          const textAreaBox = divElement.querySelector(`.${e.target.id}`);
+          textAreaBox.disabled = false;
+          divElement.querySelector(`#${e.target.id}`).value = "Guardar";
+          enableEdit = false;
+        }
+        else {
+          const editedPost = divElement.querySelector(`.${e.target.id}`).value;
+          console.log(e.target.id);
+          console.log(editedPost);
+          const db = firebase.firestore();
+          await db.collection("posts").doc(`${e.target.id}`).update({name: "Daniela",
+            recipe: editedPost,
+            fecha: "2/9/21",
+            user: "444444",});
+          enableEdit = true;
+        }
+        
+      })
+    })}
+
+  const createPost = (data, user, idPost) => {
     let template = '';
-    if (data.user === user.uid) {
+    if(data.user===user.uid){
       template = `
-      <div class="containerPosts">
+      <div class="userContainerPost">
         <div class="userContainerPost">
           <div class="headerPost">
             <p class="postName postNameDesktop">${data.name}</p>
             <p class="datePost datePostDesktop">${data.fecha}</p>
-            <input class="inputEdit inputEditDesktop" type="button" onclick="location.href='#/edit';" value="Editar"/>
+            <input id= "${idPost}" class="inputEdit inputEditDesktop" type="button" value="Editar"/>
           </div>
-           <textarea class=textAreaGray cols="10" rows="5" disabled>${data.recipe}</textarea>
-          <div class="recipe">
-          </div>
+           <textarea class = "${idPost}"  class=textAreaGray cols="10" rows="5" disabled>${data.recipe}</textarea>
           <div class="footerPost">
             <img class="like likeDesktop" src="./images/like.png" alt="">
             <img class="delete deleteDesktop" src="./images/delete.png" alt="">
@@ -40,7 +129,7 @@ export const functionPost = () => {
       `;
     } else {
       template = `
-        <div class="userContainerPost">
+        <div class= "containerPosts">
           <div class="headerPost">
             <p class="postName postNameDesktop">${data.name}</p>
             <p class="datePost datePostDesktop">${data.fecha}</p>
@@ -52,61 +141,16 @@ export const functionPost = () => {
             <img class="like" src="./images/like.png" alt="">
           </div>
         </div>
-      `;
+      `
     }
 
     return template;
-  };
-
-  const showPost = (data) => {
-    if (data.length) {
-      let addHtml = '';
-      data.forEach((posts) => {
-        const postData = posts.data();
-        addHtml = createPost(postData) + addHtml;
-      });
-      const divcontainerPost = document.createElement('div');
-      divcontainerPost.innerHTML = addHtml;
-      divElement.querySelector('#containerPost').innerHTML = '';
-      divElement.querySelector('#containerPost').appendChild(divcontainerPost);
-    }
-  };
-
-  // función para imprimir los post en la página
-  const loadPost = async () => {
-    const db = firebase.firestore();
-    await db.collection('post')
-      .get()
-      .orderBy('fecha', 'desc')
-      .then((snapshot) => {
-        showPost(snapshot.docs);
-      });
-  };
-
-  inputPostUser.addEventListener('click', async () => {
-    divElement.querySelector('#containerPost').innerHTML = '';
-    const recipes = divElement.querySelector('#recipePostear').value;
-    const fecha = new Date();
-    const date = `${fecha.getDate()}/${(fecha.getMonth() + 1)}/${fecha.getFullYear()}`;
-    const user = firebase.auth().currentUser;
-    const db = firebase.firestore();
-    await db.collection('post').add({
-      name: user.displayName,
-      recipe: recipes,
-      fecha: date,
-      user: user.uid,
-    })
-      .then(() => {
-        divElement.querySelector('#recipePostear').value = '';
-        loadPost();
-      })
-      .catch(() => {
-        document.querySelector('#errorPost').innerHTML = '❌ Error inesperado, intente de nuevo';
-      });
-  });
-
-  document.addEventListener('DOMContentLoaded', () => {
-    loadPost();
-  });
+  }
+  
+searchInput.addEventListener('keyup', (e) => {
+   let search = e.target.value;
+   console.log(search);
+ });
+//loadPost();
   return divElement;
-};
+}
